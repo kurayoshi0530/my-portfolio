79bf733bc28722fd38b7485a96d191752c5fa513
kurayoshi19760530 <11172ky@gmail.com>
2021-11-15 10:42:53 +0900

    index.js修正

--- js/index.js
+++ js/index.js
@@ -1,4 +1,8 @@
 // ---------- タイトルを１文字づつ表示させるアニメーションの設定 ----------
+<<<<<<< HEAD
+=======
+
+>>>>>>> jsファイルをjsフォルダーに移動
 // アニメーションさせたいクラス
 var container = $(".titleAnime");
 // アニメーションスピード
@@ -27,26 +31,5 @@ setInterval(function() {
 
 // ---------- フェードインでページを表示させるアニメーションの設定 ----------
 $(function() {
-    $('body').fadeIn(1500); //2秒かけてフェードインで表示(数字が少なくなると早く表示される)
+    $('body').fadeIn(2000); //2秒かけてフェードインで表示(数字が少なくなると早く表示される)
 });
-
-// ---------- マウスカーソルのアニメーション設定 ----------
-//準備
-let cursorR = 4;  //カーソルの半径
-const cursor = document.getElementById('cursor');  //カーソル用のdivを取得
-
-//上記のdivタグをマウスに追従させる処理
-document.addEventListener('mousemove', function (e) {
-    cursor.style.transform = 'translate(' + e.clientX + 'px, ' + e.clientY + 'px)';
-});
-
-//リンクにホバー時はクラスをつける
-const linkElem = document.querySelectorAll('a');
-for (let i = 0; i < linkElem.length; i++) {
-    linkElem[i].addEventListener('mouseover', function (e) {
-        cursor.classList.add('hov_');
-    });
-    linkElem[i].addEventListener('mouseout', function (e) {
-        cursor.classList.remove('hov_');
-    });
-}
